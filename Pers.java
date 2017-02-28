a57dfcb39e5b7b43cf3c902919436b3b232d230e
marius <asdasdaf@gmail.com>
2017-02-28 09:39:25 +0100

    as
    af

---
+++ Pers.java
@@ -0,0 +1,15 @@
+/**
+ * Created by ${NikolayKanchev} on 2/28/2017.
+ */
+public class Pers {
+    private String name;
+    private int age;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
